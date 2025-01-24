661b1542a571d53a9ec1a9f70fb68e1167e3bd2b
Sabrina Paschini <sabrina.paschini@hotmail.com>
2025-01-24 11:29:38 -0300

    interpolação

--- src/app/sell-control/sell-control.component.ts
+++ src/app/sell-control/sell-control.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sell-control',
   templateUrl: './sell-control.component.html',
-  styleUrls: ['./sell-control.component.css']
+  styleUrls: ['./sell-control.component.css'],
 })
 export class SellControlComponent implements OnInit {
+  meta: number = 0;
+  dias: number = 31;
+  divisao: number = 0;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.meta = 50000;
+    this.dias = 31;
+    this.calcularDivisao();
   }
 
+  async calcularDivisao() {
+    this.divisao = await this.calculoPorDia(this.meta, this.dias);
+  }
+
+  async calculoPorDia(meta: number, dias: number): Promise<number> {
+    return meta / dias; 
+  }
 }
