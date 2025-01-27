7ea08a80f3d847c30268524c1c8fb9eaa47b2c5a
Sabrina Paschini <sabrina.paschini@hotmail.com>
2025-01-27 11:09:28 -0300

    two way biding + css changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SellControlComponent } from './sell-control/sell-control.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SellControlComponent
+    SellControlComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sell-control/sell-control.component.ts
+++ src/app/sell-control/sell-control.component.ts
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sell-control.component.css'],
 })
 export class SellControlComponent implements OnInit {
+
   meta: number = 0;
   dias: number = 31;
   divisao: number = 0;
+  diaVigente: number = 0;
+  quantidadeItens:number = 0 ; 
 
   constructor() {}
 
@@ -16,14 +19,19 @@ export class SellControlComponent implements OnInit {
     this.meta = 50000;
     this.dias = 31;
     this.calcularDivisao();
+    this.diaVigente = 1;
+    this.quantidadeItens = 0; 
   }
 
   async calcularDivisao() {
     this.divisao = await this.calculoPorDia(this.meta, this.dias);
-    
   }
 
   async calculoPorDia(meta: number, dias: number): Promise<number> {
-    return meta / dias; 
+    return meta / dias;
   }
-}
+
+  adicionaItem(){
+     this.quantidadeItens
+    
+  }}
